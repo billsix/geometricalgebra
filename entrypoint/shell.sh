c57b80d3c4b8e6ea8387eec4692a74ef699cad00
William Emerison Six <billsix@gmail.com>
2025-11-24 11:43:06 -0500

    Revert "make pip install not try to connect to the internet"
    This reverts commit 78ab23664fff08ae1ab50470bee59517496c1669.

--- entrypoint/shell.sh
+++ entrypoint/shell.sh
@@ -1,3 +1,3 @@
 cd /geometricalgebra/
-python3 -m pip install --no-deps --no-index -e . --break-system-packages --root-user-action=ignore
+python3 -m pip install --no-deps -e . --break-system-packages --root-user-action=ignore
 exec bash
